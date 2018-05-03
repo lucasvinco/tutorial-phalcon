271b12269e1d6b919a423388967326f25bf066ca
Lucas Vinco <lucasvinco@hotmail.com>
2018-05-03 19:47:19 -0300

    Add sql to create users table

---
+++ sql/create_users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+    `id`    int(10)     unsigned NOT NULL AUTO_INCREMENT,
+    `name`  varchar(70)          NOT NULL,
+    `email` varchar(70)          NOT NULL,
+
+    PRIMARY KEY (`id`)
+);
